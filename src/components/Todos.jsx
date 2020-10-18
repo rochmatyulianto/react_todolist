456f3ed021aa3c12055ea28c01f2b6eaeddeaafe
rochmatyulianto <72734069+rochmatyulianto@users.noreply.github.com>
2020-10-18 12:48:31 +0700

    complete a todo

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -4,17 +4,26 @@ import PropTypes from "prop-types";
 
 import Todo from "./Todo";
 
-const Todos = ({ todos }) => {
+const Todos = ({ todos, completeTodo }) => {
   return (
     <section className="todos">
       {todos.map((todo, index) => {
-        return <Todo key={index} text={todo.text} />;
+        return (
+          <Todo
+            key={index}
+            text={todo.text}
+            isCompleted={todo.isCompleted}
+            completeTodo={completeTodo}
+            index={index}
+          />
+        );
       })}
     </section>
   );
 };
 
 Todos.propTypes = {
-  todos: PropTypes.arrayOf(PropTypes.shape({ text: PropTypes.string }))
+  todos: PropTypes.arrayOf(PropTypes.shape({ text: PropTypes.string })),
+  completeTodo: PropTypes.func.isRequired
 };
 export default Todos;
