273f266b834f6c18fc9ec704fcf7f459a55142af
rochmatyulianto <72734069+rochmatyulianto@users.noreply.github.com>
2020-10-20 13:37:39 +0700

    create button css module

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import PropTypes from "prop-types";
+import cx from "classnames";
+
+import styles from "./button.module.css";
 
 const Button = ({ text, onClick, color, align }) => {
-  const className = [
+  /*  const className = [
     "header-btn",
     color === "black" && "main-black-color",
     color === "red" && "main-red-color",
     align === "left" && "align-left",
     align === "right" && "align-right"
-  ].join(" ");
+  ].join(" "); */
+  const classNames = cx(styles.headerBtn, {
+    [styles.mainBlackColor]: color === "black",
+    [styles.mainRedColor]: color === "red",
+    [styles.alignLeft]: align === "left",
+    [styles.alignRight]: align === "right"
+  });
+
   return (
-    <button className={className} onClick={onClick}>
+    <button className={classNames} onClick={onClick}>
       {text}
     </button>
   );
