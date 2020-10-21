1735a3bdc64be25f81e66d1a1048567c965df929
rochmatyulianto <72734069+rochmatyulianto@users.noreply.github.com>
2020-10-21 13:46:46 +0700

    create css module for todo,todos and todoform

--- src/components/Todos/Todos.jsx
+++ src/components/Todos/Todos.jsx
@@ -2,11 +2,13 @@ import React from "react";
 
 import PropTypes from "prop-types";
 
-import Todo from "./Todo";
+import Todo from "../Todo/Todo";
+
+import styles from "./todos.module.css";
 
 const Todos = ({ todos, completeTodo }) => {
   return (
-    <section className="todos">
+    <section className={styles.todos}>
       {todos.length > 0 &&
         todos.map((todo, index) => {
           return (
@@ -20,10 +22,10 @@ const Todos = ({ todos, completeTodo }) => {
           );
         })}
       {todos.length === 0 && (
-        <div className="todo-placeholder-text">
+        <div className={styles.todoPlaceholderText}>
           add todo by clicking{" "}
-          <span className="add-button-placeholder-text"> Add</span> buttton on
-          the left corner{" "}
+          <span className={styles.addButtonPlaceholderText}> Add</span> buttton
+          on the left corner{" "}
         </div>
       )}
     </section>
